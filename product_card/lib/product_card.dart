c65dca848f13cb438f90bd08c0d2b882a5a94aee
angga1518 <erlanggamuhammad01@gmail.com>
2020-08-02 22:19:32 +0700

    add product card

--- product_card/lib/product_card.dart
+++ product_card/lib/product_card.dart
@@ -1,20 +1,130 @@
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
+  bool notifOn = false;
+
+  void changeNotifStatus() {
+    notifOn = !notifOn;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         // notifikasi
+        AnimatedContainer(
+          duration: Duration(milliseconds: 500),
+          width: 150,
+          margin: EdgeInsets.only(left: 50, top: 20),
+          height: (notifOn) ? 373 : 350,
+          decoration: BoxDecoration(
+              color: Colors.lightGreen,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16))),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              "Notifikasi",
+              style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
         Container(
           margin: EdgeInsets.all(20),
           width: 200,
-          height: 250,
+          height: 350,
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16), color: Colors.red),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[Text("atas"), Text("bawah")],
+              border: Border.all(color: Colors.orange, width: 4),
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.white),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Image(
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      "https://cdn.britannica.com/17/196817-050-6A15DAC3/vegetables.jpg",
+                    )),
+                Text("data"),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      width: 180,
+                      height: 30,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.red[400], width: 3)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            height: 30,
+                            width: 30,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 112,
+                            child: Center(
+                                child: Text(
+                              "0",
+                              style: TextStyle(fontSize: 20),
+                            )),
+                          ),
+                          Container(
+                            child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.remove,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            height: 30,
+                            width: 30,
+                            color: Colors.red,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 180,
+                      height: 50,
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(16),
+                            bottomRight: Radius.circular(16)),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: Icon(
+                            Icons.add_shopping_cart,
+                            color: Colors.white,
+                          ),
+                          color: Colors.red[400],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         )
       ],
